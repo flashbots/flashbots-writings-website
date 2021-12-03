3feabe7eb2c958939e762823a1d3e1a7c60165f7
ryry79261 <ryanjnoble@gmail.com>
2021-12-04 00:35:37 +0200

    Fixed env name

--- scripts/setup-cname.js
+++ scripts/setup-cname.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 var fs = require('fs');
 
-fs.writeFile('./static/CNAME', process.env.TARGET_URL, function (err) {
+fs.writeFile('./static/CNAME', process.env.REACT_APP_TARGET_URL, function (err) {
   if (err) throw err;
   console.log('CNAME created ');
 }); 
