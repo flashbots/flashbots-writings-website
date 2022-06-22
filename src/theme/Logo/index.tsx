c46a1b03148566a549162100eea5aafe0e8aae05
RyRy79261 <ryanjnoble@gmail.com>
2022-06-22 11:55:17 +0200

    Logo & fonts

---
+++ src/theme/Logo/index.tsx
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import type {Props} from '@theme/Logo';
+
+import Link from '@docusaurus/Link';
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import {useThemeConfig} from '@docusaurus/theme-common';
+
+const Logo = (props: Props): JSX.Element => {
+  const {
+    siteConfig: {title},
+  } = useDocusaurusContext();
+  const {
+    navbar: {title: navbarTitle, logo = {src: ''}},
+  } = useThemeConfig();
+
+  const {imageClassName, titleClassName, ...propsRest} = props;
+  const logoLink = useBaseUrl(logo.href || '/');
+  const sources = {
+    light: useBaseUrl(logo.src),
+    dark: useBaseUrl(logo.srcDark || logo.src),
+  };
+
+  return (
+    <Link
+      to={logoLink}
+      {...propsRest}
+      {...(logo.target && {target: logo.target})}>
+
+        {/* Disabled config image for explict Logo SVG */}
+
+      {/* {logo.src && (
+        <ThemedImage
+          className={imageClassName}
+          sources={sources}
+          alt={logo.alt || navbarTitle || title}
+        />
+      )} */}
+      <svg width="12" height="14" viewBox="0 0 12 14">
+        <path fillRule="evenodd" clipRule="evenodd" d="M7.07289 0L7.91879 0.483459L6.631 4.76674H11.5178L4.19989 12.679L3.35398 12.1956L4.72089 7.64736H0L7.07289 0ZM5.20514 6.03608L6.33835 2.26697L2.28701 6.64736H5.02136L5.20514 6.03608ZM4.93407 10.4124L9.23077 5.76674H6.33078L6.16271 6.3243L4.93407 10.4124Z" fill="#0D0D0D"></path>
+      </svg>
+      {navbarTitle != null && <b className={titleClassName}>{navbarTitle}</b>}
+    </Link>
+  );
+};
+
+export default Logo;
