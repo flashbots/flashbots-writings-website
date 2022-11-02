9bc6865024af22caa7c6cc0cc0c85b6038f00e96
RyRy79261 <ryanjnoble@gmail.com>
2022-11-02 13:27:32 +0200

    Using router

--- src/components/useRedirect.tsx
+++ src/components/useRedirect.tsx
@@ -1,17 +1,17 @@
-import { useLocation,  useHistory, Redirect } from '@docusaurus/router';
+import React, { useEffect } from 'react';
+import { useLocation,  useHistory,  } from '@docusaurus/router';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import React, { ReactNode, useEffect, useState } from 'react';
 
 export const useRedirect = () => {  
   const extractRootSegments = ["/writings/", "/research/"]
   const { pathname,  } = useLocation()
   const base = useBaseUrl("/")
+  const history = useHistory()
 
   useEffect(() => {
     const found = extractRootSegments.find(redirectItem => (pathname.includes(redirectItem)))
     if (found) {
-      window.location.href = `${base}${pathname.replace(found, '')}`
+      history.push(`${base}${pathname.replace(found, '')}`)
     }
   }, [pathname]);
-
 }
