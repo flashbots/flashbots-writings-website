d7a3e282ddcb553fdca1485f059529c37ad86512
Gabe Koscky <68292774+gkoscky@users.noreply.github.com>
2023-09-21 16:30:14 -0400

    Add ETHGlobal Hackathon banner (#121)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -167,6 +167,12 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
         additionalLanguages: ["solidity"]
+      },
+      announcementBar: {
+        id: "ETHGlobal",
+        content:
+          '<span>🍎</span><a href="https://collective.flashbots.net/t/ethglobal-ny-hackathon-sept-22-24/2413" target="_blank" rel="noreferrer">Participate in the Flashbots hackathon track at ETHGlobal NY, Sept 22 - 24</a><span>🍎</span>',
+        isCloseable: false,
       }
     })
 })
