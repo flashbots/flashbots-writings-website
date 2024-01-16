149764824a4420644c24feae4d9e8d684d0efa9c
Gabe Koscky <68292774+gkoscky@users.noreply.github.com>
2024-01-16 02:01:04 -0500

    Fix regression
    Labels should only appear if there's a label

--- src/components/DuneDashboard/DuneDashboard.js
+++ src/components/DuneDashboard/DuneDashboard.js
@@ -7,9 +7,11 @@ export default function DuneDashboard({source, label, aspectRatio}) {
   return (
     <div className={styles.DuneDashboard} style={{"--aspect-ratio": aspectRatio}}>
       <iframe src={source} className={styles.root} />
-      <div className={styles.labels}>
-        {label && <div>{label}</div>}
-      </div>
+      {label && 
+        <div className={styles.labels}>
+          <div>{label}</div>
+        </div>
+      }
     </div>
   );
 }
