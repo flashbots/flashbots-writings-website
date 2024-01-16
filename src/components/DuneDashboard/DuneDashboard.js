d8168bbe992c19134dbc7bdd2102245ca2ea5ee7
Gabe Koscky <68292774+gkoscky@users.noreply.github.com>
2024-01-16 01:57:01 -0500

    Tweaks to Dashboard Component
    More customizable CSS and better support for markup in the labels

--- src/components/DuneDashboard/DuneDashboard.js
+++ src/components/DuneDashboard/DuneDashboard.js
@@ -7,7 +7,9 @@ export default function DuneDashboard({source, label, aspectRatio}) {
   return (
     <div className={styles.DuneDashboard} style={{"--aspect-ratio": aspectRatio}}>
       <iframe src={source} className={styles.root} />
-      {label && <span>{label}</span>}
+      <div className={styles.labels}>
+        {label && <div>{label}</div>}
+      </div>
     </div>
   );
 }
